801d1ec1071bbe935e74a61bbf66c86858c00f8e
tjgaylord123 <gaylord_trevor@hotmail.com>
2014-04-06 12:07:28 -0600

    Restructiring the project topology; consolidating the storage libraries into one and adding the second module.

---
+++ MovieMiner/MovieMiner.Interfaces/Storage/IStorageClient.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace MovieMiner.Interfaces.Storage
+{
+    public interface IStorageClient
+    {
+        Task<bool> WriteFileToStorageAsync(string filename, byte[] fileContent);
+    }
+}
