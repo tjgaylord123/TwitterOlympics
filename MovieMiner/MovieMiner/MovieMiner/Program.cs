f687735be905b24cd304dfcdca7d269d03409610
tjgaylord123 <gaylord_trevor@hotmail.com>
2014-05-04 12:01:04 -0600

    Adding better readable diagnostics to the program running time.

--- MovieMiner/MovieMiner/MovieMiner/Program.cs
+++ MovieMiner/MovieMiner/MovieMiner/Program.cs
@@ -12,11 +12,14 @@ namespace MovieMiner.Console
             System.Console.WriteLine("Starting stopwatch...");
             Stopwatch stopwatch = new Stopwatch();
             stopwatch.Start();
+
             DiscoverModules discoverModules = new DiscoverModules();
             discoverModules.Start();
+
             stopwatch.Stop();
 
-            System.Console.WriteLine("Elapsed minutes: {0}.{1}", stopwatch.Elapsed.TotalMinutes, Environment.NewLine);
+            System.Console.WriteLine("Elapsed time - {0}:{1}:{2}.{3}", stopwatch.Elapsed.Hours,
+                stopwatch.Elapsed.Minutes, stopwatch.Elapsed.Seconds, Environment.NewLine);
             System.Console.WriteLine("Press enter to quit...");
             System.Console.ReadLine();
         }
